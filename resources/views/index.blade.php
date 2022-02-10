73172e099f25c61ba135a3592a7adc153b9cc5b6
takaya-47 <local.s.s.47@gmail.com>
2022-02-10 21:24:03 +0900

    トップページのサンプルを作成

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MyBBS</title>
+</head>
+<body>
+    <h1>My BBS</h1>
+    <ul>
+        <li>Title</li>
+        <li>Title</li>
+        <li>Title</li>
+    </ul>
+</body>
+</html>
