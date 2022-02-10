0840441ba08cedac3e4a8a6227ec2841f1bab159
takaya-47 <local.s.s.47@gmail.com>
2022-02-10 21:32:53 +0900

    cssファイルの作成とスタイル調整

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,13 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>MyBBS</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h1>My BBS</h1>
-    <ul>
-        <li>Title</li>
-        <li>Title</li>
-        <li>Title</li>
-    </ul>
+    <div class="container">
+        <h1>My BBS</h1>
+        <ul>
+            <li>Title</li>
+            <li>Title</li>
+            <li>Title</li>
+        </ul>
+    </div>
 </body>
 </html>
