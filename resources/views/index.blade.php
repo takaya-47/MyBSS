fb566fcacc180e06206ebfc581b475c068f64779
takaya-47 <local.s.s.47@gmail.com>
2022-02-12 11:27:21 +0900

    繰り返し処理に変更

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,15 +8,18 @@
     <title>MyBBS</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
     <div class="container">
         <h1>My BBS</h1>
         <ul>
-            {{-- <li><?php echo htmlspecialchars($posts[0], ENT_QUOTES, 'UTF-8'); ?></li> --}}
-            <li>{{ $posts[0] }}</li>
-            <li>{{ $posts[1] }}</li>
-            <li>{{ $posts[2] }}</li>
+            @forelse ($posts as $post)
+                <li>{{ $post }}</li>
+            @empty
+                <li>No posts yet!</li>
+            @endforelse
         </ul>
     </div>
 </body>
+
 </html>
