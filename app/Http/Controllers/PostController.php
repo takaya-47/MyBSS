9c908f99ac6dcf0878dfee703f5a38182deb91d1
takaya-47 <local.s.s.47@gmail.com>
2022-02-12 13:05:16 +0900

    詳細ページを作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -22,7 +22,7 @@ class PostController extends Controller
     // 投稿詳細
     public function show($id)
     {
-        return view('show')
+        return view('posts.show')
             ->with(['post' => $this->posts[$id]]);
     }
 
