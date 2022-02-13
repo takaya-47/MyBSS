8f37f9ea6c4d7f35e9c60f7a2258a349cbce8260
takaya-47 <local.s.s.47@gmail.com>
2022-02-13 18:53:54 +0900

    indexページとshowページの書き換え

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,29 +1,14 @@
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>MyBBS</title>
-    <link rel="stylesheet" href="{{ url('css/style.css') }}">
-</head>
-
-<body>
-    <div class="container">
-        <h1>My BBS</h1>
-        <ul>
-            @forelse ($posts as $index => $post)
-                <li>
-                    <a href="{{ route('posts.show', $index) }}">
-                        {{ $post }}
-                    </a>
-                </li>
-            @empty
-                <li>No posts yet!</li>
-            @endforelse
-        </ul>
-    </div>
-</body>
-
-</html>
+<x-layout>
+    <h1>My BBS</h1>
+    <ul>
+        @forelse ($posts as $index => $post)
+            <li>
+                <a href="{{ route('posts.show', $index) }}">
+                    {{ $post }}
+                </a>
+            </li>
+        @empty
+            <li>No posts yet!</li>
+        @endforelse
+    </ul>
+</x-layout>
