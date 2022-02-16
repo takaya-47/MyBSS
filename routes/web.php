904cb55859aabc9eb058ca22940a073464793d07
takaya-47 <local.s.s.47@gmail.com>
2022-02-16 22:23:02 +0900

    投稿ページのルーティング作成

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::get('/', [PostController::class, 'index'])
 // 投稿詳細
 Route::get('/posts/{post}', [PostController::class, 'show'])
     ->name('posts.show');
+
+// 投稿詳細
+Route::get('/posts/create', [PostController::class, 'create'])
+    ->name('posts.create');
