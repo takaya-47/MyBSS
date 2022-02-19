bb67eaae05b44215bf12869c4cae08614ce055b2
takaya-47 <local.s.s.47@gmail.com>
2022-02-19 12:55:37 +0900

    投稿編集ページのルーティング

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,7 @@ Route::get('/posts/create', [PostController::class, 'create'])
 Route::post('/posts/store', [PostController::class, 'store'])
     ->name('posts.store');
 
+// 投稿編集
+Route::get('/posts/{post}/edit', [PostController::class, 'edit'])
+    ->name('posts.edit')
+    ->where('post', '[0-9]+');
