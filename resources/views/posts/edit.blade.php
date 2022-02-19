7fda5ceded9eacf9fb3439ecd61d1e27d4a4086f
takaya-47 <local.s.s.47@gmail.com>
2022-02-19 12:56:59 +0900

    投稿詳細ページに編集ページへのリンク作成、編集ページ作成

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,39 @@
+<x-layout>
+    <x-slot name="title">
+        Edit Post - My BBS
+    </x-slot>
+    <div class="back-link">
+        &laquo; <a href="{{ route('posts.show', $post) }}">Back</a>
+    </div>
+    <h1>Edit Post</h1>
+    <form action="" method="post">
+        {{-- LaravelにおけるCSRF対策 --}}
+        @csrf
+        {{-- <input> を直接 <label> の内側に入れる場合は関連付けが明確なので、 for および id 属性は必要ないけど一応。 --}}
+        <div class="form-group">
+            <label for="title">
+                Title
+                <input type="text" name="title" id="title" value="{{ old('title', $post->title) }}">
+            </label>
+            {{-- // バリデーションに引っかかった時のエラーメッセージ --}}
+            @error('title')
+                <div class="error">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="form-group">
+            <label for="body">
+                Body
+                <textarea name="body" id="body">{{ old('body', $post->body) }}</textarea>
+            </label>
+            {{-- // バリデーションに引っかかった時のエラーメッセージ --}}
+            @error('body')
+                <div class="error">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="form-button">
+            <button>
+                Update
+            </button>
+        </div>
+    </form>
+</x-layout>
