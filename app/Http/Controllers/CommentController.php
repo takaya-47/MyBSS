bbaa5c6fb7e541432b6ed752e4a127fdcbd25c70
takaya-47 <local.s.s.47@gmail.com>
2022-02-20 14:02:37 +0900

    とりあえずコメント投稿時のバリデーションを実装

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -9,6 +9,11 @@ use App\Models\Comment;
 class CommentController extends Controller
 {
     public function store(Request $request, Post $post) {
+        // TODO: Postコントローラーのようにバリデーション用のクラスを作る
+        $request->validate([
+            'body' => 'required',
+        ]);
+
         $comment = new Comment();
         $comment->post_id = $post->id;
         $comment->body = $request->body;
