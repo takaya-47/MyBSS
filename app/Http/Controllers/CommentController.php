01d4c2d44c0d0b06aa1033632a0c86b5cef9450a
takaya-47 <local.s.s.47@gmail.com>
2022-02-20 13:47:02 +0900

    コメント生成のメソッドを記述

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\Comment;
 
 class CommentController extends Controller
 {
-    //
+    public function store(Request $request, Post $post) {
+        $comment = new Comment();
+        $comment->post_id = $post->id;
+        $comment->body = $request->body;
+        $comment->save();
+
+        return redirect()
+            ->route('posts.show', $post);
+    }
 }
